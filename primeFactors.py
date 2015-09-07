7ea90584b64e84f4a678df7549d4c08ae4f9323b
halilbay <halilbay91@gmail.com>
2015-09-07 21:59:09 +0300

    prime factors

--- primeFactors.py
+++ primeFactors.py
@@ -1,3 +1,19 @@
 __author__ = 'halilbay'
 
 
+def is_prime(number):
+    flag = True
+    for i in range(2, number):
+        if i != number and number % i == 0:
+            flag = False
+
+    return flag
+
+
+def prime_factor(number):
+    arr = []
+    for i in range(2, number):
+        if number % i == 0 and is_prime(i):
+
+            arr.append(i)
+    return arr
